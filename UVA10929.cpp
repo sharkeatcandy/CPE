101006ea6ac17268d757906ad314658e0d0932d4
Shark <sc113943@yahoo.com.tw>
2016-07-25 11:49:35 +0800

    Update UVA10929.cpp
    add text

--- UVA10929.cpp
+++ UVA10929.cpp
@@ -4,13 +4,14 @@ using namespace std;
 
 int main()
 {
-   int a;
-   while(cin>>a){
-       if(a==0)break;
-       int b=a%11;
-       int c=a/11;
-       if(b==0 && c!=0)cout<<"ok"<<endl;
-       if(b!=0) cout<<"qq"<<endl<<b<<endl<<c<<endl;
+   int a;//宣告被除數
+   while(cin>>a)//當輸入變數時執行下列函式
+   {
+       if(a==0)break;//當變數為0時中止函式
+       int b=a%11;//宣告餘數
+       int c=a/11;//宣告商
+       if(b==0 && c!=0)cout<<a<<" is a multiple of 11."<<endl;//判斷餘數為零，且商不為零，輸出數字被整除訊息
+       if(b!=0) cout<<a<<" is not a multiple of 11."<<endl;//判斷餘數不為零，輸出數字無法被整除訊息
    }
    
    return 0;
