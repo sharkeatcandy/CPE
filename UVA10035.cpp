49befb4e46b9129dacfd4486c7bf8fc46762747d
Shark <sc113943@yahoo.com.tw>
2016-07-25 20:26:26 +0800

    Update UVA10035.cpp

--- UVA10035.cpp
+++ UVA10035.cpp
@@ -15,17 +15,17 @@ int main()
    int max;//宣告最後位數
    while(cin>>a>>b)//當輸入變數時執行下列函式
    {
-    if(a[0]=='0' && b[0]=='0')break;
-    int count=0;
-    la=strlen(a);
-    lb=strlen(b);
-    if(la-lb>=0){number=(la-lb);max=la;}
+    if(a[0]=='0' && b[0]=='0')break;//當兩數字皆為0時停止函式
+    int count=0;//宣告進位次數
+    la=strlen(a);//設定位數
+    lb=strlen(b);//設定位數
+    if(la-lb>=0){number=(la-lb);max=la;}//判斷共有幾位數需要運算
     else {number=(lb-la);max=lb;}
-        for(int i=max;i>=number;i--)
+        for(int i=max;i>=number;i--)//進行運算
         {
-            if((a[i]+b[i]-96)>=10)count++;
+            if((a[i]+b[i]-96)>=10)count++;//進位一次count+1
         }
-    if(count==0)cout<<"No carry operation."<<endl;
+    if(count==0)cout<<"No carry operation."<<endl;//判斷count並輸出結果
     else cout<<count<<" carry operation."<<endl;
    
    }
