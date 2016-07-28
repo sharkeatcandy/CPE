ef0a996dfa4e303a9ddd0a9e07afe37d3a3a17bf
Shark <sc113943@yahoo.com.tw>
2016-07-28 21:44:15 +0800

    Update UVA10035.cpp
    bug fixed

--- UVA10035.cpp
+++ UVA10035.cpp
@@ -19,15 +19,33 @@ int main()
     int count=0;//宣告進位次數
     la=strlen(a);//設定位數
     lb=strlen(b);//設定位數
-    if(la-lb>=0){number=(la-lb);max=la;}//判斷共有幾位數需要運算
-    else {number=(lb-la);max=lb;}
-        for(int i=max;i>=number;i--)//進行運算
+    if(la-lb>=0)
+    {
+    	number=la-lb;max=la;
+    	for(int i=max-1;i>=0;i--)//進行運算
         {
-            if((a[i]+b[i]-96)>=10)count++;//進位一次count+1
+            if((a[i]+b[i-number]-96)>=10 || a[i]>57)//判斷相加是否大於10(由於是char變數，減48後才等於0)
+            {
+            	count++;//進位一次count+1
+            	a[i-1]+=1;//進位
+            }
         }
+    }//判斷共有幾位數需要運算
+    else 
+    {
+    	number=lb-la;max=lb;
+        for(int i=max-1;i>=0;i--)//進行運算
+        {
+            if((a[i-number]+b[i]-96)>=10 || b[i]>57)
+            {
+            	count++;//進位一次count+1
+            	b[i-1]+=1;//進位
+            }
+        }
+    }
     if(count==0)cout<<"No carry operation."<<endl;//判斷count並輸出結果
-    else cout<<count<<" carry operation."<<endl;
-   
+    else if(count==1) cout<<count<<" carry operation."<<endl;
+   	else cout<<count<<" carry operations."<<endl;
    }
    return 0;
 }
